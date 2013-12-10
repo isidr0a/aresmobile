0a0edd6cdb307bff22efda939388f1d7552656cb
isidroa <isidro.a.garcia@gmail.com>
2013-12-10 17:52:46 -0430

    testing

--- aresmobile.js
+++ aresmobile.js
@@ -104,12 +104,14 @@ var AM	={
 	*************************/
 	install:function (){
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) { 
-			var entry=fileSystem.root; 
+			var entry=fileSystem.root;
+			alert(fileSystem.name);
 			entry.getDirectory("AM", {create: true, exclusive: false}, function (dir) {
 					AM.glovar.folderMaster=dir;
+					alert('creating directory'+AM.glovar.folderMaster.name);
 				},
 				function (error) {
-					console.log("Error creating directory "+error.code)
+					alert("Error creating directory "+error.code);
 				}
 			)
 		} , null); 
